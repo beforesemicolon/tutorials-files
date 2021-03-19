3434c813543ccc279f05244aff6ff2b938d233fa
Elson Correia <13211046+ECorreia45@users.noreply.github.com>
2021-03-19 04:32:57 -0400

    capacity max

--- stack2.js
+++ stack2.js
@@ -4,7 +4,7 @@ class Stack {
   #capacity = null;
   
   constructor(capacity = null) {
-    this.#capacity = capacity;
+    this.#capacity = Math.max(Number(capacity), 0) || null;
   }
 
   get size() {
