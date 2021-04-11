e4c797ed7266a888b6b37dad4baa159dbdc00136
Elson Correia <13211046+ECorreia45@users.noreply.github.com>
2021-04-11 17:44:57 -0400

    new double linked list

---
+++ double-linked-list2.js
@@ -0,0 +1,85 @@
+class DoubleLinkedList extends LinkedList {
+  #size = 0;
+  tail = null;
+  
+  get size() {
+    return this.#size;
+  }
+  
+  createElement(value) {
+    return {value, next: null, prev: null}
+  }
+  
+  push(item) {
+    const element = this.createElement(item);
+    
+    if(this.head === null) {
+      this.head = element;
+      this.tail = element;
+    } else {
+      this.tail.next = element;
+      element.prev = this.tail;
+      this.tail = element;
+    }
+    
+    this.#size += 1;
+    return this.size;
+  }
+  
+  insert(item, index = 0) {
+    if (index < 0 || index > this.size) return;
+    
+    if(index === this.size) {
+      return this.push(item);
+    }
+
+    const element = this.createElement(item);
+
+    if (index === 0) {
+      element.next = this.head;
+      this.head.prev = element;
+      this.head = element;
+    } else {
+      let previous = this.head;
+      
+      for(let i = 0; i < index - 1; i++) {
+        previous = previous.next;
+      }
+
+      element.next = previous.next;
+      previous.next.prev = element;
+      previous.next = element;
+      element.prev = previous;
+    }
+
+    this.#size += 1;
+    return this.size;
+  }
+  
+  remove(index = 0) {
+    if (index < 0 || index >= this.size) return null;
+
+    let removedElement = this.head;
+
+    if (index === 0) {
+      this.head.next.prev = null;
+      this.head = this.head.next;
+    } else if(index === this.size - 1) {
+      this.tail.prev.next = null;
+      this.tail = this.tail.prev;
+    } else {
+      let previous = this.head;
+
+      for(let i = 0; i < index - 1; i++) {
+        previous = previous.next;
+      }
+
+      removedElement = previous.next;
+      previous.next = removedElement.next;
+      removedElement.next.prev = previous;
+    }
+
+    this.#size -= 1;
+    return removedElement.value;
+  }
+}
