d23269b5a0e927a2ff4cb674845fe0b6ee655ff7
Elson Correia <13211046+ECorreia45@users.noreply.github.com>
2021-04-29 10:46:47 -0400

    simpler static

--- set.js
+++ set.js
@@ -33,7 +33,7 @@ class SetExtended extends Set {
     return differenceSet;
   }
 
-  intersect(set) {
+  intersection(set) {
     const intersectionSet = new SetExtended();
 
     if (!this.#isValidSet(set)) return intersectionSet;
@@ -63,41 +63,9 @@ class StaticSet extends SetExtended {
   constructor(items) {
     super(items);
     
-    delete this.add;
-    delete this.delete;
-    delete this.clear;
-  }
-  
-  // OPTIONAL
-  // to also make the items in the set readonly
-  // overide the forEach, keys, values and entries as well
-  #items = () => [...this].map(item => {
-  
-     if(item instanceof Map) {
-       item = new Map(Array.from(item.entries()))
-     } else if(item instanceof Set) {
-       item = new Set(Array.from(item.values()))
-     } else if(item && typeof item === 'object') {
-       Object.freeze(item);
-     } 
-
-     return item;
-  });
-
-  forEach(cb) {
-    this.#items().forEach(cb)
-  }
-
-  keys() {
-    return this.#items().values();
-  }
-  
-  values() {
-    return this.#items().values();
-  }
-
-  entries() {
-    return this.#items().map(item => ([item, item]));
+    this.add = undefined;
+    this.delete = undefined;
+    this.clear = undefined;
   }
 }
 
