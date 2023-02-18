099b753110cc5003a258f68ce512c60d70e3b0bb
Elson Correia <design@elsoncorreia.com>
2023-02-18 05:54:39 -0500

    remove node-server as it has become a template

--- node-server-setup/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  testEnvironment: 'node',
-  testRegex: './src/.*\\.(test|spec)?\\.(js|ts)$',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  roots: ['<rootDir>/src'],
-};
